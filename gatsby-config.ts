1147f871f01ccdad22aae89d2b93c098688776a1
mathiasfc <mathias.falci@gmail.com>
2024-11-19 03:28:35 -0300

    allow absolute paths when importing sass file

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,22 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `new-react-challenge`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    {
+      resolve: "gatsby-plugin-sass",
+      options: {
+        sassOptions: {
+          includePaths: [`${__dirname}/src/styles`],
+        },
+      },
+    },
+  ],
 };
 
 export default config;
