0b657e26802c3a5ef6058c600314408e7a3108d9
mathiasfc <mathias.falci@gmail.com>
2024-11-19 03:27:19 -0300

    add global styles to gatsby-browser.tsx

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "./src/styles/global.css";
 
 const queryClient = new QueryClient();
 
 export const wrapRootElement = ({ element }) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      {element}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{element}</QueryClientProvider>
   );
 };
