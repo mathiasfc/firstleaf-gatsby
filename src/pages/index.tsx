e004bf2a31a65f527af3ed7276bd4a70c452729f
mathiasfc <mathias.falci@gmail.com>
2024-11-19 03:38:58 -0300

    formats the pages/index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,31 +3,31 @@ import Countdown from "../components/Countdown";
 import ProductGrid from "../components/ProductGrid";
 
 const pageStyles = {
-    color: "#232129",
-    padding: "96px",
-    fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+  color: "#232129",
+  padding: "96px",
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+};
 const headingStyles = {
-    marginTop: 0,
-    marginBottom: 32,
-    maxWidth: 320,
-}
+  marginTop: 0,
+  marginBottom: 32,
+  maxWidth: 320,
+};
 
 const HomePage = (): JSX.Element => (
-    <main style={pageStyles}>
-        <h1 style={headingStyles}>Choose a challenge:</h1>
-        <ul>
-            <li>
-                <a href="/countries">Countries</a>
-            </li>
-            <li>
-                <a href="/bucket">Bucket</a>
-            </li>
-            <li>
-                <a href="/promo">Promo</a>
-            </li>
-        </ul>
-    </main>
+  <main style={pageStyles}>
+    <h1 style={headingStyles}>Choose a challenge:</h1>
+    <ul>
+      <li>
+        <a href="/countries">Countries</a>
+      </li>
+      <li>
+        <a href="/bucket">Bucket</a>
+      </li>
+      <li>
+        <a href="/promo">Promo</a>
+      </li>
+    </ul>
+  </main>
 );
 
 export default HomePage;
