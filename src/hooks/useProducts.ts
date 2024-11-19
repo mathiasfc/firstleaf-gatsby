acb62c8358a44f1237bd39a9b74e454e486b6263
mathiasfc <mathias.falci@gmail.com>
2024-11-19 03:31:44 -0300

    fix the loading state of useProducts custom hook

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,15 +1,16 @@
-import { useQuery } from 'react-query';
-import { Product } from '../types/Products';
+import { useQuery } from "react-query";
+import { Product } from "../types/Products";
 
-export function useProducts() { 
-  const { data, isLoading } = useQuery('products', async () => {
-    const products =  await fetch('https://my-json-server.typicode.com/YofretRios/jsondetails/products')
-      .then(res => res.json()) as Array<Product>;
+export function useProducts() {
+  const { data, isLoading } = useQuery("products", async () => {
+    const products = (await fetch(
+      "https://my-json-server.typicode.com/YofretRios/jsondetails/products"
+    ).then((res) => res.json())) as Array<Product>;
 
-    window.localStorage.setItem('products', JSON.stringify(products));
-    
+    window.localStorage.setItem("products", JSON.stringify(products));
+
     return products;
-  }, { initialData: [] });
+  });
 
   return { data, isLoading };
 }
