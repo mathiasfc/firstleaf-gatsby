f4a3fb584a6435cfbc1fe233c377088ea2055937
mathiasfc <mathias.falci@gmail.com>
2024-11-19 03:40:18 -0300

    fix jest tests by mocking svg and scss files

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "\\.module\\.scss$": "<rootDir>/test/__mocks__/file-mock.js",
+    "\\.svg$": "<rootDir>/test/__mocks__/file-mock.js",
+  },
   testMatch: [
-    '**/__tests__/**/*.(ts|tsx|js|jsx)',
-    '**/?(*.)+(spec|test).(ts|tsx|js|jsx)',
+    "**/__tests__/**/*.(ts|tsx|js|jsx)",
+    "**/?(*.)+(spec|test).(ts|tsx|js|jsx)",
   ],
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.json" }],
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
